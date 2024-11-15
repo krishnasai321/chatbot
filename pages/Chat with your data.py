3b80f8b79a1bf53c57e57dd445fa3553fb6456d6
krishnasai321 <saikrishna321@gmail.com>
2024-11-15 06:12:27 +0530

    updated chat with your data, requirements.txt

--- pages/Chat with your data.py
+++ pages/Chat with your data.py
@@ -3,17 +3,20 @@ import openai
 from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import pandas as pd
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+
 
 # Custom title for the page
-st.set_page_config(page_title="Chat with your Txt file", page_icon="📝")
+st.set_page_config(page_title="Chat with your data", page_icon="📝")
 
 # Title of the app
-st.title("📝 File Q&A with Langchain and OpenAI")
+st.title("📝 Data Q&A with Langchain and OpenAI")
 
 # File uploader and question input
-uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
+uploaded_file = st.file_uploader("Upload your dataset", type=("csv"))
 question = st.text_input(
-    "Ask something about the article",
+    "Ask something about the dataset",
     placeholder="Can you give me a short summary?",
     disabled=not uploaded_file,
 )
@@ -23,33 +26,14 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 # Langchain setup
 if uploaded_file and question and openai_api_key:
-    # Read the uploaded file content
-    article = uploaded_file.read().decode()
+    df = pd.read_csv(uploaded_file)
 
     # Set the OpenAI API key for Langchain
     openai.api_key = openai_api_key
 
-    # Create a prompt template
-    prompt_template = """You are a helpful assistant. Below is an article and a question. Please provide a detailed answer based on the article.
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True,allow_dangerous_code=True)
 
-    Article:
-    {article}
-
-    Question: {question}
-
-    Answer:"""
-
-    # Create the prompt template for Langchain
-    prompt = PromptTemplate(input_variables=["article", "question"], template=prompt_template)
-
-    # Set up the OpenAI model through Langchain
-    llm = OpenAI(temperature=0.7)
-
-    # Create the LLMChain with Langchain
-    chain = LLMChain(llm=llm, prompt=prompt)
-
-    # Run the chain to generate an answer
-    answer = chain.run(article=article, question=question)
+    answer = agent.invoke(question)
 
     # Display the result
     st.write("### Answer")
